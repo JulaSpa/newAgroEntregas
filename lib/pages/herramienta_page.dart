51560c47bcfd9e6763330c0dc7e07315506b66dc
JulaSpa <julianspataro@hotmail.com>
2023-12-13 10:26:18 -0300

    new mods

--- lib/pages/herramienta_page.dart
+++ lib/pages/herramienta_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 class Tool extends StatefulWidget {
   const Tool({super.key});
@@ -9,6 +10,175 @@ class Tool extends StatefulWidget {
 }
 
 class _ToolState extends State<Tool> {
+  String? linkNoticias;
+  String? linkHDFS;
+  String? linkHyV;
+  String? linkCA;
+  String? linkCondiciones;
+  String? linkCamiones;
+  String? linkRef;
+  @override
+  void initState() {
+    super.initState();
+    // Obtener los valores de SharedPreferences
+    fetchData();
+  }
+
+  void fetchData() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Noticias'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resHdfs = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Hfds'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resHyV = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Hvc'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resCA = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/CA'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resCond = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Condiciones'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resCamiones = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Camiones'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+      final resRef = await http.get(
+        Uri.parse('http://net.entreganet.com/RestServiceImpl.svc/Referencias'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+              "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+        },
+      );
+
+      if (response.statusCode == 200 ||
+          resHdfs.statusCode == 200 ||
+          resHyV.statusCode == 200 ||
+          resCA.statusCode == 200 ||
+          resCond.statusCode == 200 ||
+          resCamiones.statusCode == 200 ||
+          resRef.statusCode == 200) {
+        final exp = RegExp(r'<[^>]*>', multiLine: true, caseSensitive: true);
+        //REF xmls
+        print("Referencias:");
+        final jsonContentRef = resRef.body.replaceAll(exp, "");
+        try {
+          final dataRef = jsonContentRef;
+          linkRef = dataRef;
+          print(linkRef);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //CAMIONES xmlns
+        print("camiones:");
+        final jsonContentCam = resCamiones.body.replaceAll(exp, "");
+        try {
+          final dataCam = jsonContentCam;
+          linkCamiones = dataCam;
+          print(linkCamiones);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //CONDICIONES xmlns
+
+        print("CONDICIONES:");
+        final jsonContentCond = resCond.body.replaceAll(exp, '');
+
+        try {
+          final dataCond = jsonContentCond;
+          linkCondiciones = dataCond;
+          print(linkCondiciones);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //COSTOS Y ACONDICIONAMIENTOS xmls
+        final jsonContentCA = resCA.body.replaceAll(exp, '');
+        print("CA:");
+        try {
+          final dataCa = jsonContentCA;
+          linkCA = dataCa.replaceAll('"', '');
+          print(linkCA);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //HORARIOS Y VIGENCIA DE CUPOS xmls
+        final jsonContenthyv = resHyV.body.replaceAll(exp, '');
+        print("HyV:");
+        try {
+          final datahyv = jsonContenthyv;
+          linkHyV = datahyv.replaceAll('"', '');
+          print(linkHyV);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //HORARIOS FINDES: xmls
+        final jsonContenhdfs = resHdfs.body.replaceAll(exp, '');
+        print("HDFS:");
+        try {
+          final datahdfs = jsonContenhdfs;
+          linkHDFS = datahdfs;
+          print(linkHDFS);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+        //NOTICIAS DENTRO DE xmlns
+
+        final jsonContent = response.body.replaceAll(exp, '');
+        print("noticias");
+        try {
+          final dataNoti = jsonContent;
+          linkNoticias = dataNoti;
+          print(linkNoticias);
+        } catch (e) {
+          print('Error: No se encontró el inicio del JSON en la respuesta.');
+        }
+      } else {
+        print('Error de red. Código de estado: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -47,7 +217,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch("https://agroentregas.com.ar/");
+                              launch(linkNoticias!);
                             },
                             child: Column(
                               children: [
@@ -76,8 +246,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/condiciones-de-recibo-en-planta");
+                              launch(linkCondiciones!);
                             },
                             child: Column(
                               children: [
@@ -119,8 +288,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/total-de-camiones-en-el-puerto");
+                              launch(linkCamiones!);
                             },
                             child: Column(
                               children: [
@@ -150,8 +318,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/tipos-de-camiones");
+                              launch(linkRef!);
                             },
                             child: Column(
                               children: [
@@ -188,8 +355,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/herramientas/costos-de-servicio-en-acondicionadoras-y-puertos");
+                              launch(linkCA!);
                             },
                             child: Column(
                               children: [
@@ -219,8 +385,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/herramientas/horarios-de-fin-de-semana-y-feriados");
+                              launch(linkHDFS!);
                             },
                             child: Column(
                               children: [
@@ -257,8 +422,7 @@ class _ToolState extends State<Tool> {
                         Expanded(
                           child: GestureDetector(
                             onTap: () {
-                              launch(
-                                  "https://agroentregas.com.ar/herramientas/horarios-de-vigencia-de-los-cupos");
+                              launch(linkHyV!);
                             },
                             child: Column(
                               children: [
